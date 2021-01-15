4ba9efec0ea9c231c012dfea8482a9c1b22ccbf8
nionis <stivennoni77@gmail.com>
2021-01-15 22:13:37 +0200

    test

--- src/components/views/Pdf.js
+++ src/components/views/Pdf.js
@@ -2,7 +2,14 @@ import React from 'react'
 import { EmbedPdf } from '../elements'
 
 const Pdf = document => {
-  return () => <EmbedPdf src={require(`../assets/documents/${document}`)} />
+  return () => (
+    <>
+      <a href={require(`../assets/documents/${document}`)} target="_blank">
+        Download PDF
+      </a>
+      <EmbedPdf src={require(`../assets/documents/${document}`)} />
+    </>
+  )
 }
 
 export default Pdf
