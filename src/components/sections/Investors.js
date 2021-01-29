e09a0abfd6d0caa2bb27d581215dbdb606c65d4c
jjperezaguinaga <me@jjperezaguinaga.com>
2021-01-29 05:13:38 +0000

    Updating investors section

--- src/components/sections/Investors.js
+++ src/components/sections/Investors.js
@@ -111,17 +111,17 @@ class Investors extends React.Component {
                   />
                 </a>
               </li>
-              {/* <li className="reveal-from-bottom" data-reveal-delay="150">
-                <a href="https://www.bitcoinsuisse.com/" target="_blank" rel="noopener noreferrer">
+              <li className="reveal-from-bottom" data-reveal-delay="150">
+                <a href="https://www.vendetta.capital/" target="_blank" rel="noopener noreferrer">
                   <Image
-                    src={require('@hoprnet/assets/images/partners/bitcoinsuisse.png')}
-                    alt="Bitcoin Suisse Logo"
+                    src={require('@hoprnet/assets/images/partners/vendetta_capital.png')}
+                    alt="Vendetta Capital Logo"
                     className={imgClasses}
                     width={124}
                     height={24}
                   />
                 </a>
-              </li> */}
+              </li>
             </ul>
             {showQuestion ? (
               <div className="question">
